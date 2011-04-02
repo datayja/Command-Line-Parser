4740e2c0841519a34c8a873ba43d1ec7b1bf84cd
Pavel Lisa <pablick@gmail.com>
2011-04-02 23:52:26 +0200

    Initial commit

---
+++ DU2_PARSER/du2_parser.h
@@ -0,0 +1,18 @@
+//
+//  du2_parser.h
+//  DU2_PARSER
+//
+//  Created by Pavel Lisa on 2.4.11.
+//  Copyright 2011 MFF UK. All rights reserved.
+//
+
+class Parser {
+	<#instance variables#>
+	
+public:
+	Parser() {
+		
+	}
+	
+	~Parser() {}
+};
