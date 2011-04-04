3645e1ea561940e58cdbe11e874538c18d3a6916
Pavel Lisa <pablick@gmail.com>
2011-04-04 18:39:35 +0200

    functor

---
+++ DU2_PARSER/parser_value_functor.h
@@ -0,0 +1,14 @@
+//
+//  value_policy.h
+//  DU2_PARSER
+//
+//  Created by Pavel Lisa on 3.4.11.
+//  Copyright 2011 MFF UK. All rights reserved.
+//
+
+class ParserValueFunctor {
+private:
+	void * stored_target;
+public:
+	void operator()(void * in_target);
+};
